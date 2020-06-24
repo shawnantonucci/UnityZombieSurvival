47679d343c0b4c75e87d262356adfb6fd6b10c62
Shawn Antonucci <shawnantonucci2018@gmail.com>
2020-06-24 18:19:30 -0400

    setup git

---
+++ Assets/_Scripts/PlayerMovement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+
+    public Rigidbody2D rb;
+
+    public Camera cam;
+
+    Vector2 movement;
+    Vector2 mousePos;
+
+    private Vector2 lookDirection;
+    private float angle;
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
+    }
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+
+        lookDirection = mousePos - rb.position;
+        angle = Mathf.Atan2(lookDirection.y, lookDirection.x) * Mathf.Rad2Deg - 90f;
+        rb.rotation = angle;
+    }
+}
